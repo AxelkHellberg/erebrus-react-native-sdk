b691c59d34f77e973e80f7b1e482337826744362
evadsh3ll <keshwanitejas@gmail.com>
2025-06-17 17:42:49 +0530

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultTheme = exports.StatusCard = exports.ConnectionButton = exports.ClientCreator = exports.useVPN = exports.VPNProvider = void 0;
+exports.defaultTheme = exports.Auth = exports.StatusCard = exports.ConnectionButton = exports.ClientCreator = exports.useVPN = exports.VPNProvider = void 0;
 var VPNProvider_1 = require("./components/VPNProvider");
 Object.defineProperty(exports, "VPNProvider", { enumerable: true, get: function () { return VPNProvider_1.VPNProvider; } });
 Object.defineProperty(exports, "useVPN", { enumerable: true, get: function () { return VPNProvider_1.useVPN; } });
@@ -10,6 +10,8 @@ var ConnectionButton_1 = require("./components/ConnectionButton");
 Object.defineProperty(exports, "ConnectionButton", { enumerable: true, get: function () { return ConnectionButton_1.ConnectionButton; } });
 var StatusCard_1 = require("./components/StatusCard");
 Object.defineProperty(exports, "StatusCard", { enumerable: true, get: function () { return StatusCard_1.StatusCard; } });
+var Auth_1 = require("./components/Auth");
+Object.defineProperty(exports, "Auth", { enumerable: true, get: function () { return Auth_1.Auth; } });
 exports.defaultTheme = {
     background: '#f5f5f5',
     surface: '#ffffff',
